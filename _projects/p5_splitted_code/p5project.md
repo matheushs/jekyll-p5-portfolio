07d191288bdeb7d2cedb502c5340cd9c7d2b7207
Matheus H. Soares <matt.h.soares@gmail.com>
2018-01-17 19:49:11 -0200

    Might have fixed everything

--- _projects/p5_splitted_code/p5project.md
+++ _projects/p5_splitted_code/p5project.md
@@ -1,26 +1,14 @@
 ---
 layout: post
-title:  "P5 sketch splitted in several files"
+title:  "Rainy City"
 date:   2016-08-01
 thumbnail: particles_thumbnail.png
 dependencies:
     - p5
 ---
 
-It is often convenient to split a complex javascript file in different files.
-In this case, make sure to import all the files:
-
-{%highlight html %}
-<div id="sketch-holder">
-    <script type="text/javascript" src="sketch/Particle.js"></script>
-    <script type="text/javascript" src="sketch/ParticleSystem.js"></script>
-    <script type="text/javascript" src="sketch/sketch.js"></script>
-</div>
-{% endhighlight %}
-
-This particle system is [one of p5.js official examples](https://p5js.org/examples/simulate-particle-system.html):
-<div id="sketch-holder">
-    <script type="text/javascript" src="sketch/Particle.js"></script>
-    <script type="text/javascript" src="sketch/ParticleSystem.js"></script>
-    <script type="text/javascript" src="sketch/sketch.js"></script>
+Supposedly a moody rainy city, WIP:
+<div id="sketch-holder" style="height: 400px">
+  <script type="text/javascript" src="sketch/drop.js"></script>
+  <script type="text/javascript" src="sketch/sketch.js"></script>
 </div>
--- _projects/p5_splitted_code/sketch/sketch.js
+++ _projects/p5_splitted_code/sketch/sketch.js
@@ -1,13 +1,47 @@
-var system;
+let rain = [];
+let bg;
 
 function setup() {
-  var canvas = createCanvas(720, 400);
-  canvas.parent('sketch-holder');
-  system = new ParticleSystem(createVector(width/2, 50));
+  // put setup code here
+  myDiv = document.getElementById('sketch-holder');
+  console.log(myDiv.offsetWidth);
+  console.log(myDiv.offsetHeight);
+  myCanvas = createCanvas(myDiv.offsetWidth, myDiv.offsetHeight);
+  myCanvas.parent('sketch-holder');
+
+  gravity = createVector(0, 9.8);
+  for (let i = 0; i < 500; i++) {
+    rain.push(new Drop());
+  }
+}
+
+function preload() {
+  bg = loadImage('bg.png');
+}
+
+function windowResized() {
+  resizeCanvas(myDiv.offsetWidth, myDiv.offsetHeight);
+
 }
 
 function draw() {
-  background(51);
-  system.addParticle();
-  system.run();
+  background(12, 110, 110);
+  image(bg, 0, 0, myDiv.offsetWidth, myDiv.offsetHeight);
+
+  push();
+
+  for (drop of rain) {
+    drop.applyForce(gravity);
+    drop.update();
+    drop.render();
+  }
+  pop();
+
+  for (let i = rain.length - 1; i >= 0; i--) {
+    if (rain[i].offScreen()) {
+      rain[i].pos.x = random(width);
+      rain[i].pos.y = random(-200, -100);
+    }
+  }
+  pop();
 }
